253589d23346a4640ead236f0ccf0e6d94b00f13
xmhscratch <xmhscratch@gmail.com>
2024-12-20 22:26:43 +0700

    update

--- system/cli.js
+++ system/cli.js
@@ -1,87 +1,138 @@
-import inquirer from 'inquirer'
-import { spawn } from 'child_process'
+import inquirer from "inquirer";
+import { spawn } from "child_process";
 
-inquirer
-  .prompt([
+const selectAction = async () =>
+  inquirer.prompt([
     {
-      name: 'action',
-      message: 'DevServer action',
-      type: 'list',
+      name: "action",
+      message: "DevServer action",
+      type: "list",
       choices: [
-        { name: 'Initialize microservice', value: 'init' },
-        { name: 'Start dev server', value: 'start:server' },
-        { name: 'Build production', value: 'build:server' },
-        { name: 'Build docker', value: 'build:docker' },
-      ]
+        { name: "Init service", value: "init-service" },
+        { name: "Start dev server", value: "start-dev" },
+        { name: "Generate nuxt files", value: "build-nuxt" },
+        { name: "Build docker image", value: "build-docker" },
+      ],
     },
-  ])
-  .then(async ({ action }) => {
-    switch (action) {
-      case 'init': case 'start:server': case 'build:server': {
-        inquirer
-          .prompt([
-            {
-              name: 'target',
-              message: 'Target microservice',
-              type: 'list',
-              choices: [
-                { name: 'CMS', value: 'cms' },
-                { name: 'Auth', value: 'auth' },
-                { name: 'Services -> Dashboard', value: 'ms-dashboard' },
-              ]
-            }
-          ])
-          .then(async ({ target }) => {
-            const cmdString = `./node_modules/.bin/dotenvx run -- npm run --workspace=${target}`
-            switch (action) {
-              case 'init': {
-                await spawn('sh', ['-c', `npm install`], { stdio: 'inherit' }, Promise.resolve)
-                await spawn('sh', ['-c', `${cmdString} postinstall`], { stdio: 'inherit' }, Promise.resolve)
-                break
-              }
-              case 'server:dev': {
-                await spawn('sh', ['-c', `${cmdString} dev`], { stdio: 'inherit' }, Promise.resolve)
-                break
-              }
-              case 'server:build': {
-                await spawn('sh', ['-c', `${cmdString} build`], { stdio: 'inherit' }, Promise.resolve)
-                await spawn('sh', ['-c', `${cmdString} generate`], { stdio: 'inherit' }, Promise.resolve)
-                await spawn('sh', ['-c', `${cmdString} preview`], { stdio: 'inherit' }, Promise.resolve)
-                break
-              }
-              default: break
-            }
-          })
-        break
-      }
-      case 'build:docker': {
-        inquirer
-          .prompt([
-            {
-              name: 'target',
-              message: 'Docker images to build',
-              type: 'list',
-              choices: [
-                { name: 'GoNode', value: 'dp-gonode' },
-                { name: 'System Environment', value: 'dp-sysenv' },
-                { name: 'Microservice', value: 'dp-microservice' },
-                { name: 'RabbitMQ', value: 'dp-rabbitmq' },
-              ]
-            }
-          ])
-          .then(async ({ target }) => {
-            const cmdString = `./node_modules/.bin/dotenvx run -- npm run --workspace=${target}`
-            await spawn('sh', ['-c', `${cmdString} build`], { stdio: 'inherit' }, Promise.resolve)
-          })
-        break
-      }
-      default: break
+  ]);
+
+const selectServiceTarget = async (action) =>
+  inquirer.prompt([
+    {
+      name: "target",
+      message: "Target microservice",
+      type: "list",
+      choices: [
+        { name: "CMS", value: "services.cms" },
+        { name: "Account", value: "services.account" },
+        { name: "Dashboard", value: "services.dashboard" },
+      ],
+    },
+  ]);
+
+const selectDockerImageTarget = async (action) =>
+  inquirer.prompt([
+    {
+      name: "target",
+      message: "Docker images to build",
+      type: "list",
+      choices: [
+        { name: "GoNode", value: "deploy.gonode" },
+        { name: "System Environment", value: "deploy.sysenv" },
+        { name: "Microservice", value: "deploy.microservice" },
+        { name: "RabbitMQ", value: "deploy.rabbitmq" },
+      ],
+    },
+  ]);
+
+const execCommand = async (action, target) => {
+  const npmCommand = `./node_modules/.bin/dotenvx run -- npm`;
+  switch (action) {
+    case "init-service": {
+      await spawn(
+        "sh",
+        ["-c", `npm install`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} run --workspace=${target} postinstall`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      break;
     }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+    case "start-dev": {
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} run --workspace=${target} dev`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      break;
     }
-  })
+    case "build-nuxt": {
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} run --workspace=${target} build`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} run --workspace=${target} generate`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} run --workspace=${target} preview`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      break;
+    }
+    case "build-docker": {
+      await spawn(
+        "sh",
+        ["-c", `${npmCommand} --workspace=${target} build`],
+        { stdio: "inherit" },
+        Promise.resolve
+      );
+      break;
+    }
+    default:
+      break;
+  }
+};
+
+const errorHandler = (error) => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+};
+
+(async () => {
+  const { action } = await selectAction().catch(errorHandler);
+  switch (action) {
+    case "init-service":
+    case "start-dev":
+    case "build-nuxt": {
+      const { target } = await selectServiceTarget(action).catch(errorHandler);
+      await execCommand(action, target).catch(errorHandler);
+      break;
+    }
+    case "build-docker": {
+      const { target } = await selectDockerImageTarget(action).catch(
+        errorHandler
+      );
+      await execCommand(action, target).catch(errorHandler);
+      break;
+    }
+    default:
+      break;
+  }
+})();
--- system/dashboard/nuxt.config.ts
+++
@@ -1,3 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import baseConfig from '../../base.config';
-export default defineNuxtConfig(<object>baseConfig);
